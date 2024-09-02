e3ea401be7402ee50abd0694b71a130798e21b3c
AkashSahoo123 <158769858+AkashSahoo123@users.noreply.github.com>
2024-09-03 03:09:43 +0530

    socket link

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -9,7 +9,7 @@ export const SocketContextProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    setSocket(io("http://localhost:4000"));
+    setSocket(io("https://housey-2.onrender.com"));
   }, []);
 
   useEffect(() => {
