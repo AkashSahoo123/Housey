2cd0e78e73722cbf9caf793bc67bf0a5acb3d02e
AkashSahoo123 <158769858+AkashSahoo123@users.noreply.github.com>
2024-09-03 04:52:47 +0530

    Update apiRequest.js

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,8 +1,19 @@
 import axios from 'axios';
 
-const apiRequest=axios.create({
-    baseURL:"https://housey-6f1v.onrender.com/api",
-    withCredentials:true
-})
+const apiRequest = axios.create({
+    baseURL: "https://housey-6f1v.onrender.com/api",
+    withCredentials: true,
+});
+
+// Add a request interceptor to include the JWT token in every request
+apiRequest.interceptors.request.use(config => {
+    const token = JSON.parse(localStorage.getItem("user"))?.token;
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+}, error => {
+    return Promise.reject(error);
+});
 
 export default apiRequest;
