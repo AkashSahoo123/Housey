521ccfaae9d389270f10ca8de09b4a07f2e1bdf3
AkashSahoo123 <158769858+AkashSahoo123@users.noreply.github.com>
2024-09-03 04:11:24 +0530

    Update apiRequest.js

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiRequest=axios.create({
-    baseURL:"https://housey-6f1v.onrender.com/api",
+    baseURL:"https://housey-2.onrender.com/",
     withCredentials:true
 })
 
